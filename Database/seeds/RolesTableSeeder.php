17674ed02fe1ca31e4740bf5fb1a96e32b4262ea
saniswandika <saniswandikaa@gmail.com>
2023-09-16 15:48:03 +0700

    commit pertama ygy

---
+++ Database/seeds/RolesTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::truncate();
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'employee']);
+    }
+}
